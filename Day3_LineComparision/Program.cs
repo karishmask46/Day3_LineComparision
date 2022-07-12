d1e92178d2e7189478bbeb98a1b975c8a5c564ac
karishmask46 <karishmask.15999@gmail.com>
2022-07-12 16:49:41 +0530

    Comparing lengths

--- Day3_LineComparision/Program.cs
+++ Day3_LineComparision/Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision");
-            UC2_EqualLengths comp = new UC2_EqualLengths();
+            UC3_LengthsComparision comp = new UC3_LengthsComparision();
             double Ans1 = comp.LengthLine1();
             double Ans2 = comp.LengthLine2();
 
@@ -14,8 +14,16 @@
             {
                 Console.WriteLine("lengths are equal");
             }
-            else
+            else if (Ans1 < Ans2)
             {
+                Console.WriteLine("one length is less than another length");
+            }
+            else if (Ans1 > Ans2)
+            {
+                Console.WriteLine("one leghth is greater than another length");
+            }
+            else 
+            { 
                 Console.WriteLine("lengths are not equal");
             }
             
