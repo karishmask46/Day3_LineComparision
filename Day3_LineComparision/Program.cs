09bf80371cc43cf02ce883913f8e72f30aa1932a
karishmask46 <karishmask.15999@gmail.com>
2022-07-12 16:16:41 +0530

    calculating length of line

---
+++ Day3_LineComparision/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Day3_LineComparision
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision");
+            UC1_LineLengths comp = new UC1_LineLengths();
+            comp.LengthLine1();
+        }
+    }
+}
---
+++ Day3_LineComparision/UC1_LineLengths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_LineComparision
+{
+    internal class UC1_LineLengths
+    {
+           public void LengthLine1()
+            {
+                Console.WriteLine(" enter the value of x1 : ");
+                int x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the value of x2 : ");
+                int x2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the value of y1 : ");
+                int y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the value of y2 : ");
+                int y2 = Convert.ToInt32(Console.ReadLine());
+                Double length1 = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+                Console.WriteLine(length1);
+            }
+        }
+    }
+
